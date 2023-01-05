7d264d4ef92eb56e0584cb1d0fada907ec6d97d3
Matthew Hague <matthewhague@zoho.com>
2023-01-05 11:52:08 +0000

    refactor(tests): parameterize downloader tests

--- app/src/test/java/app/crossword/yourealwaysbe/net/DownloadersTest.java
+++ app/src/test/java/app/crossword/yourealwaysbe/net/DownloadersTest.java
@@ -2,13 +2,14 @@
 package app.crossword.yourealwaysbe.net;
 
 import java.time.LocalDate;
+import java.util.AbstractList;
 import java.util.Collections;
 import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.junit.runners.Parameterized.Parameters;
+import org.junit.runners.Parameterized;
 
 import android.content.Context;
 import android.content.SharedPreferences;
@@ -16,36 +17,53 @@ import android.content.SharedPreferences;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.*;
 
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(Parameterized.class)
 public class DownloadersTest {
 
     private static final String DEFAULT_NAME = "puzzle";
 
-    @Mock
-    Context context;
+    /**
+     * Parameters are the list of downloaders
+     *
+     * Gotten from the Downloaders class (needs mocking of context and
+     * prefs). Returned as a list of pairs: (internalName, downloader).
+     */
+    @Parameters(name = "Downloader {0}")
+    public static Iterable<Object[]> downloaders() {
+        Context context = mock(Context.class);
+        SharedPreferences prefs = mock(SharedPreferences.class);
+        initialiseMocks(context, prefs);
+        final List<Downloader> downloaders
+            = (new Downloaders(context, prefs)).getDownloaders();
+        return new AbstractList<Object[]>() {
+            @Override
+            public int size() { return downloaders.size(); }
 
-    @Mock
-    SharedPreferences prefs;
+            @Override
+            public Object[] get(int i) {
+                Downloader d = downloaders.get(i);
+                return new Object[] { d.getInternalName(), d };
+            }
+        };
+    }
+
+    private Downloader downloader;
+
+    public DownloadersTest(String name, Downloader downloader) {
+        this.downloader = downloader;
+    }
 
     @Test
     public void testDownloaders() {
-        initialiseMocks();
-
-        List<Downloader> downloaders
-            = (new Downloaders(context, prefs)).getDownloaders();
-
-        for (Downloader downloader : downloaders) {
-            System.out.println(
-                "Testing " + downloader.getInternalName() + " download."
-            );
-            LocalDate latest = downloader.getLatestDate();
-            Downloader.DownloadResult res =
-                downloader.download(latest, Collections.emptySet());
-            assertTrue(res.isSuccess());
-        }
+        LocalDate latest = downloader.getLatestDate();
+        Downloader.DownloadResult res =
+            downloader.download(latest, Collections.emptySet());
+        assertTrue(res.isSuccess());
     }
 
-    private void initialiseMocks() {
+    private static void initialiseMocks(
+        Context context, SharedPreferences prefs
+    ) {
         // say yes to all downloaders
         when(prefs.getBoolean(anyString(), anyBoolean())).thenReturn(true);
         // except dummy custom downloader
